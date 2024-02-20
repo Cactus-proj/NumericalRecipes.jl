e008dd89250393d5dfcfdc8149b13c1213ce875c
woclass <git@wo-class.cn>
2024-02-19 23:53:34 -0600

    test: Special.gammln: more tests

--- test/special.jl
+++ test/special.jl
@@ -17,7 +17,15 @@ const SF = SpecialFunctions
     @test_throws "bad arg in gammln" gammln(+0.0)
     @test_throws "bad arg in gammln" gammln(-0.0)
     @test_throws "bad arg in gammln" gammln(-1)
-    
+
     test_gammaln(NaN)
-    test_gammaln.(1:10)
+    @test_broken Inf === gammln(Inf)
+
+    test_gammaln.(1:100)
+
+    test_gammaln.(rand(eps(0.0):eps():2.0^-21,  10))
+    test_gammaln.(rand( 2.0^-21:eps():2.0,      10))
+    test_gammaln.(rand(     2.0:eps():8.0,      10))
+    test_gammaln.(rand(     8.0:2.0^58,         10))
+    test_gammaln.(rand(  2.0^58:1e300:1e308,    10))
 end
