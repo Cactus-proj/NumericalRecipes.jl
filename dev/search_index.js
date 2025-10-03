609f35bc16405cbd1b30051b37c3c97c00bbcb6c
Documenter.jl <documenter@juliadocs.github.io>
2025-10-03 05:36:10 +0000

    build based on 991a352

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NumericalRecipes","category":"page"},{"location":"#NumericalRecipes","page":"Home","title":"NumericalRecipes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NumericalRecipes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NumericalRecipes]","category":"page"}]
+[{"location":"#NumericalRecipes","page":"Home","title":"NumericalRecipes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NumericalRecipes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
